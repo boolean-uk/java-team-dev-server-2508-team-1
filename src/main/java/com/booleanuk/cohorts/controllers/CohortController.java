6c4f00f44df6ab09e0e3c928018503a586960452
Richard Persson <ricpersson00@gmail.com>
2025-09-12 09:44:00 +0200

    updated if statement

--- src/main/java/com/booleanuk/cohorts/controllers/CohortController.java
+++ src/main/java/com/booleanuk/cohorts/controllers/CohortController.java
@@ -25,7 +25,7 @@ public class CohortController {
     @GetMapping("{id}")
     public ResponseEntity<Response> getCohortById(@PathVariable int id) {
         Cohort cohort = this.cohortRepository.findById(id).orElse(null);
-        if (cohort == null) {
+        if (cohort == null || cohort.getUsers().isEmpty()) {
             ErrorResponse error = new ErrorResponse();
             error.set("not found");
             return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
