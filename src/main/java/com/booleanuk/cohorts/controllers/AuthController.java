08f3d8c63aeeb11644f014aaf80054894eca35ce
Emanuels Zaurins <emanuels.zaurins@no.experis.com>
2025-09-17 14:40:21 +0200

    I created it so that name and lastname gets from the token when loggin in

--- src/main/java/com/booleanuk/cohorts/controllers/AuthController.java
+++ src/main/java/com/booleanuk/cohorts/controllers/AuthController.java
@@ -1,5 +1,23 @@
 package com.booleanuk.cohorts.controllers;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.booleanuk.cohorts.models.ERole;
 import com.booleanuk.cohorts.models.Role;
 import com.booleanuk.cohorts.models.User;
@@ -12,20 +30,8 @@ import com.booleanuk.cohorts.repository.RoleRepository;
 import com.booleanuk.cohorts.repository.UserRepository;
 import com.booleanuk.cohorts.security.jwt.JwtUtils;
 import com.booleanuk.cohorts.security.services.UserDetailsImpl;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import jakarta.validation.Valid;
 
 //fixed issue with login.
 @CrossOrigin(origins = "*", maxAge = 3600)
